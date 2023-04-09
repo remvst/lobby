ff2c79e3bfe18f63ed4ae3f4e0a8f6b16d9cf996
remvst <remvst@gmail.com>
2023-04-09 18:40:10 -0400

    Stuff

--- lobby-server/src/storage.ts
+++ lobby-server/src/storage.ts
@@ -7,34 +7,38 @@ export interface Storage {
     deleteLobby(id: string): Promise<void>;
 }
 
-function randomDelay() {
-    return new Promise(r => setTimeout(r, Math.random() * 1000));
-}
-
 export class InMemoryStorage implements Storage {
 
     private readonly lobbies = new Map<string, string>();
+    
+    constructor(private readonly maxDelay: number) {
+        
+    }
+
+    private randomDelay() {
+        return new Promise(r => setTimeout(r, Math.random() * this.maxDelay));
+    }
 
     async getLobbies(): Promise<Lobby[]> {
-        await randomDelay();
+        await this.randomDelay();
         return Array.from(this.lobbies.values()).map(l => JSON.parse(l) as Lobby);
     }
 
     async getLobby(id: string): Promise<Lobby | null> {
-        await randomDelay();
+        await this.randomDelay();
         const serialized = this.lobbies.get(id);
         if (!serialized) return null;
         return JSON.parse(serialized) as Lobby;
     }
 
     async updateLobby(lobby: Lobby): Promise<void> {
-        await randomDelay();
+        await this.randomDelay();
         const serialized = JSON.stringify(lobby);
         this.lobbies.set(lobby.id, serialized);
     }
 
     async deleteLobby(id: string): Promise<void> {
-        await randomDelay();
+        await this.randomDelay();
         this.lobbies.delete(id);
     }
 }
